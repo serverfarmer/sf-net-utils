03b4b8807bc048d6d8f300a5bca169f1698d0d57
Tomasz Klim <github@tomaszklim.pl>
2022-11-21 12:42:08 +0100

    updated external ip providers

--- external/pia.sh
+++ external/pia.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -m 5 -H "User-Agent: Firefox" https://pol.privateinternetaccess.com/site-api/get-location-info
+curl -s -m 5 -H "User-Agent: Firefox" https://www.privateinternetaccess.com/site-api/get-location-info
